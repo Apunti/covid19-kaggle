b4c9a2769eddeb944071620b4501658d6c8c1adf
Albert Puntí Turull <46543039+Apunti@users.noreply.github.com>
2020-06-15 16:40:31 +0200

    Add files via upload

--- extract_features_refactored.py
+++ extract_features_refactored.py
@@ -29,7 +29,7 @@ import os
 
 import biobert.modeling as modeling
 import biobert.tokenization as tokenization
-import tensorflow.compat.v1 as tf
+import tensorflow as tf
 
 import numpy as np
 import pandas as pd
